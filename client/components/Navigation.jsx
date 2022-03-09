b19774012b304fccecfa7aecfacec2da4f89dd41
courtney-brown-dev-journey <emailcourtneyrosebrown@gmail.com>
2022-03-10 10:31:45 +1300

    basic navbar set up

--- client/components/Navigation.jsx
+++ client/components/Navigation.jsx
@@ -9,7 +9,7 @@ function Navigation (props) {
 
 <Navbar className="me-auto" bg="light" expand="lg">
     <Container>
-    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Brand href="#home">Alayne and Aaron</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
